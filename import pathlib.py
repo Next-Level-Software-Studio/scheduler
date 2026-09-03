e8cc423b7c27c6974b40904d72ab3383a077e891
Lumarysz <230233445+Lumarysz@users.noreply.github.com>
2026-09-03 22:46:50 +0100

    adiciona funções para obter diretórios e configurações conforme o sistema operacional

--- import pathlib.py
+++ import pathlib.py
@@ -1,10 +1,20 @@
 from pathlib import Path
 from datetime import datetime
-import platform, os, time, threading, signal
+import platform, os, time, threading, signal, subprocess
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from croniter import croniter
+def get_filesdir():
+    if platform.system() == "Linux":
+        return Path("/etc/scheduler") 
+    elif platform.system() == "Windows":
+        return Path(f"{os.environ.get('ProgramData')}\\scheduler\\files")
 
+def get_configuration():
+    if platform.system() == "Linux":
+        return Path("/etc/scheduler.conf") 
+    elif platform.system() == "Windows":
+        return Path(f"{os.environ.get('ProgramData')}\\scheduler\\scheduler.conf")
 dados = {}
 parar_execucao = threading.Event()
 
@@ -40,21 +50,22 @@ signal.signal(signal.SIGTERM, tratar_sinal_desligamento)
 signal.signal(signal.SIGINT, tratar_sinal_desligamento)
 
 def tarefa_principal_do_programa():
+    with open(get_configuration(), "r") as f:
+        for linha in f:
+            if linha.startswith("actualization_time="):
+                tempo_atualizacao = int(linha.split("actualization_time=")[1])
+                break
+        else:
+            tempo_atualizacao = 120
     while True:
-        contador += 1
-        time.sleep(4)
-
-def get_filesdir():
-    if platform.system() == "Linux":
-        return Path("/etc/scheduler") 
-    elif platform.system() == "Windows":
-        return Path(f"{os.environ.get('ProgramData')}\\scheduler\\files")
-
-def get_configuration():
-    if platform.system() == "Linux":
-        return Path("/etc/scheduler.conf") 
-    elif platform.system() == "Windows":
-        return Path(f"{os.environ.get('ProgramData')}\\scheduler\\scheduler.conf")
+        for i in dados:
+            time_code, script, proxima_execucao = dados[i]
+            if datetime.now() == proxima_execucao:
+                subprocess.run(script, shell=True)
+                hora_de_execucao = croniter(time_code, datetime.now())
+                proxima_execucao = hora_de_execucao.get_next(datetime)
+                dados[i][3] = proxima_execucao
+        time.sleep(tempo_atualizacao)
 
 Pasta_Ficheiros = get_filesdir()
 Pasta_Ficheiros.mkdir(parents=True, exist_ok=True)
@@ -84,11 +95,18 @@ for arquivo in Pasta_Ficheiros.iterdir():
         script = content[1:]
         hora_de_execucao = croniter(time_code, horas)
         proxima_execucao = hora_de_execucao.get_next(datetime)
-        dados[str(arquivo.resolve())] = [time_code, script, horas, proxima_execucao]
+        dados[str(arquivo.resolve())] = [time_code, script, proxima_execucao]
 
 def processar_ficheiro(tipo_evento, caminho_ficheiro):
-    nome = os.path.basename(caminho_ficheiro)
     caminho_abs = os.path.abspath(caminho_ficheiro)
+    with open(caminho_abs, "w") as k:
+        conteudo = k.readlines()
+    horas = datetime.now()
+    time_code = conteudo[0]
+    script = conteudo[1:]
+    hora_de_execucao = croniter(time_code, horas)
+    proxima_execucao = hora_de_execucao.get_next(datetime)
+    dados[str(arquivo.resolve())] = [time_code, script, proxima_execucao]
 
 handler = DebouncedHandler(
     callback_func=processar_ficheiro, 
